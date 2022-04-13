8956a7a5de6b320a036806d0e3867652b7344029
CEO of Identity <91491306+Iantheman-24@users.noreply.github.com>
2022-04-13 17:20:34 +0000

    Did a test run on scanner

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; // Import Scanner class
+class Main {
+  public static void main(String[] args) {
+    Scanner year = new Scanner(System.in); // Creates a Scannner Object
+    System.out.println("Choose a year");
+
+    String name = year.nextLine(); // Reads the user input (int)
+    
+  }
+}
